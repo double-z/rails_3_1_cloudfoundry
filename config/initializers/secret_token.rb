a21e337a4ee23b2a76e4a131bf2a5ebb181daf44
Katsunori Kawaguchi <hamakata@gmail.com>
2011-12-08 13:08:11 +0900

    rails new rails_3_1_cloudfoundry -T -d mysql --skip-bundle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rails31Cloudfoundry::Application.config.secret_token = '6a58807fa8d11647acb41d206d79b0a720d5ebb48d58c419b574d377f2aa0ff44daee6b59f19752c97bf461cc213945ae30b41396a3bdebfc7265a6c584f6481'
